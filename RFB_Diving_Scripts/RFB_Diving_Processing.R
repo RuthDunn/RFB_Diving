675ba0f9d5de14acd91d9f9dd713a9a24ae8a893
Ruth Dunn <ruthelizabethdunn@gmail.com>
2022-12-21 15:16:57 +0000

    Read in and edit 2019 and 2022 dive text files together.

--- RFB_Diving_Scripts/RFB_Diving_Processing.R
+++ RFB_Diving_Scripts/RFB_Diving_Processing.R
@@ -1,68 +1,11 @@
 rm(list = ls(all = TRUE))
 
 library(tidyverse) # for piping etc
-library(geosphere) # for distHaversine function
-library(lubridate) # for dmy function
-library(adehabitatHR) # for interpolation
+# library(lubridate) # for dmy function
+# library(adehabitatHR) # for interpolation
 
-# Function to designate Trip IDs:
-source('RFB_Diving_Scripts/Functions/TripID.R', echo=TRUE) # Function for assigning trip IDs; commented out for now
-
-# Function to extract statistical mode:
-getmode <- function(v) {
-  uniqv <- unique(na.omit(v))
-  uniqv[which.max(tabulate(match(v, uniqv)))]
-}
-
-files <- list.files(path = "RFB_Diving_Data/BIOT_CH_2022_AxyTrek/", pattern = "*.csv")
-files <- separate(as.data.frame(files), 1, into = "files", sep = ".csv")
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# Load raw-ish gps data ####
-
-for (j in 1:nrow(files)) {
-  
-  # j = 1 # use to test code
-  
-  # Load files
-  df.gps <- read_tsv(paste0("RFB_Diving_Data/BIOT_CH_2022_AxyTrek/", files[j,], ".txt"), col_names = F) %>%
-    dplyr::select(X1:X4) %>%
-    drop_na() %>%
-    dplyr::rename(Date = 1, Time = 2, Lat = 3, Lon = 4)
-  
-  # df.gps <- df.gps[1:10000,] # use to test code
-  
-  # Edit time format
-  df.gps$Date <- dmy(df.gps$Date)
-  df.gps$Date.Time <- as.POSIXct(paste(df.gps$Date, as.character(df.gps$Time)), format = "%Y-%m-%d %H:%M:%S", tz="UTC")
-  df.gps$Bird <- files[j,]
-  
-  # Interpolation
-  tr <- as.ltraj(data.frame(cbind(df.gps$Lon, df.gps$Lat)),
-                 df.gps$Date.Time, df.gps$Bird)
-  df.gps <- redisltraj(tr, u = 1, type = "time") # u = new step length in seconds
-  df.gps <- ld(df.gps) %>%
-    dplyr::select(x:date) %>%
-    dplyr::rename(Lon = 1, Lat = 2, Date = 3)
-  
-  # >1 km from colony = trip
-  # Determine nest coordinates (as the most common GPS)
-  nest.coords = c(getmode(df.gps$Lon), getmode(df.gps$Lat))
-  
-  # Calc dists from nest
-  df.gps$Dist.km = distHaversine(nest.coords, cbind(df.gps$Lon, df.gps$Lat))/1000
-
-  df.gps$Trip <- ifelse(df.gps$Dist.km > 1,
-                        TRUE,
-                        FALSE)
-  
-  # Trip IDs
-  df.gps <- Give.trip.IDs(df.gps)
-  
-  write_csv(df.gps, paste0("RFB_Diving_Data/CH_AxyTrek_Processed/", files[j,], "_gps.csv"))
-
-  }
+files <- as.data.frame(list.files(path = "RFB_Diving_Data/BIOT_AxyTrek_Dives_csv/", pattern = "*.csv")) %>%
+  separate(1, into = "files", sep = ".csv")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -81,9 +24,9 @@ for (i in 1:nrow(files)) {
                     mutate(DateTime = as.POSIXct(paste(Date, as.character(Time)), format = "%d/%m/%Y %H:%M:%S", tz="UTC")) %>%
                     dplyr::select(TagID, DateTime, Depth),
                   read_csv(paste0("RFB_Diving_Data/CH_AxyTrek_Processed/", files[i,], "_gps.csv")) %>%
-                    dplyr::select(Date, Lon, Lat, Trip, TripID) %>%
-                    rename("DateTime" = 1),
-                  by = "DateTime")
+                    dplyr::select(DateTime, Lon, Lat, Dist.km, Trip, TripID),
+                  by = "DateTime") %>%
+    drop_na(Lat)
   
   write_csv(df, paste0("RFB_Diving_Data/CH_AxyTrek_Processed/", files[i,], "_depth.csv"))
 }
@@ -116,14 +59,16 @@ for (i in 1:nrow(files)) {
   
   write_csv(df, paste0("RFB_Diving_Data/CH_AxyTrek_Processed/", files[i,], "_depth_corrected.csv"))
   
-  for (j in 1:length(unique(as.Date(df$DateTime)))) {
+  df <- na.omit(df)
+  
+  for (j in 1:length(unique(df$TripID))) {
     
-    datj <- unique(as.Date(df$DateTime))[j]
+    tripj <- unique(df$TripID)[j]
     
-    ggplot(subset(df, as.Date(DateTime) == datj)) +
-      geom_point(aes(x = DateTime, y = Depth_mod, col = Trip), alpha = 0.4) +
+    ggplot(subset(df, TripID == tripj)) +
+      geom_point(aes(x = DateTime, y = Depth_mod), alpha = 0.4) +
       theme_light()
     
-    ggsave(paste0("RFB_Diving_Plots/", files[i,], "_", j, "b.png"), width = 8, height = 8)
+    ggsave(paste0("RFB_Diving_Plots/", files[i,], "_", j, ".png"), width = 8, height = 8)
   }
 }
