95c8313c34a523a1e48306a0efbfca047de59fd8
Ruth Dunn <ruthelizabethdunn@gmail.com>
2022-12-21 17:01:05 +0000

    Read in gps and depth files, correct for device drift and write up plots within single loop.

--- RFB_Diving_Scripts/RFB_Diving_Processing.R
+++ RFB_Diving_Scripts/RFB_Diving_Processing.R
@@ -9,57 +9,57 @@ files <- as.data.frame(list.files(path = "RFB_Diving_Data/BIOT_AxyTrek_Dives_csv
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Load raw-ish dive data ####
+# Load raw-ish dive data
 
 # Loop through .csv files
 # Select depth cols only
 # Drop na data
 # Combine GPS data
 
-for (i in 1:nrow(files)) {
-  
-  df <- left_join(read_tsv(paste0("RFB_Diving_Data/BIOT_CH_2022_AxyTrek/", files[i,], ".csv")) %>%
-                    dplyr::select(TagID, Date, Time, Depth) %>%
-                    drop_na() %>%
-                    mutate(DateTime = as.POSIXct(paste(Date, as.character(Time)), format = "%d/%m/%Y %H:%M:%S", tz="UTC")) %>%
-                    dplyr::select(TagID, DateTime, Depth),
-                  read_csv(paste0("RFB_Diving_Data/CH_AxyTrek_Processed/", files[i,], "_gps.csv")) %>%
-                    dplyr::select(DateTime, Lon, Lat, Dist.km, Trip, TripID),
-                  by = "DateTime") %>%
-    drop_na(Lat)
-  
-  write_csv(df, paste0("RFB_Diving_Data/CH_AxyTrek_Processed/", files[i,], "_depth.csv"))
-}
+# Correct for device drift
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# Correct for device drift ####
-
-# Looks at every time point in conjunction with the next 30 to determine whether the baseline has shifted
+# Look at every time point in conjunction with the next 30
+# to determine whether the baseline has shifted
 
 k = 30  # window width for smoothing
 
 for (i in 1:nrow(files)) {
   
-  df <- read_csv(paste0("RFB_Diving_Data/CH_AxyTrek_Processed/", files[i,], "_depth.csv"))
+  # Load data ####
+  
+  df <- left_join(read_tsv(paste0("RFB_Diving_Data/BIOT_AxyTrek_Dives_csv/", files[i,], ".csv")) %>%
+                    dplyr::select(TagID, Date, Time, Depth) %>%
+                    drop_na() %>%
+                    mutate(DateTime = as.POSIXct(paste(Date, as.character(Time)), format = "%d/%m/%Y %H:%M:%S", tz="UTC")) %>%
+                    dplyr::select(TagID, DateTime, Depth),
+                  read_csv(paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[i,], "_gps.csv")) %>%
+                    dplyr::select(DateTime, Lon, Lat, Dist.km, Trip, TripID),
+                  by = "DateTime") %>%
+    drop_na(Lat)
+  
+  # Correct drift ####
   
   # Calculate rolling median depth every 30 readings
   offset <- zoo::rollapply(df$Depth, width = k, by = k, FUN = median)
   offset <- rep(offset, each = k)
-
+  
   # Match lengths of offset df and OG depth df
   dif = length(df$Depth) - length(offset)
   offset = c(offset, rep(tail(offset, 1), dif))
-
+  
   # Perform zero-offset correction on depth data
   new_series = df$Depth - offset
   new_series[new_series<0] = 0  # correct meaningless negative depths
   new_series[(length(new_series)-dif):length(new_series)]
   df$Depth_mod = new_series
   
-  write_csv(df, paste0("RFB_Diving_Data/CH_AxyTrek_Processed/", files[i,], "_depth_corrected.csv"))
+  # Write file ####
+  
+  write_csv(df, paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[i,], "_depth_corrected.csv"))
   
   df <- na.omit(df)
+  
+  # Save plots of each trip ####
   
   for (j in 1:length(unique(df$TripID))) {
     
