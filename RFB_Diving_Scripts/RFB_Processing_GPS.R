99ec2b1030cdfc9db7fd19a9b8695da3d955b005
Ruth Dunn <Ruth@HPspectrex3601.win.pet.hw.ac.uk>
2023-01-25 15:05:21 +0000

    Read in raw GPS data and process foraging trips

--- RFB_Diving_Scripts/RFB_Processing_GPS.R
+++ RFB_Diving_Scripts/RFB_Processing_GPS.R
@@ -34,29 +34,11 @@ for (j in 1:nrow(files)) {
   df.gps <- read_tsv(paste0("RFB_Diving_Data/BIOT_AxyTrek_GPS_txt/", files[j,], ".txt"), col_names = F) %>%
     dplyr::select(X1:X4) %>%
     drop_na() %>%
-    dplyr::rename(Date = 1, Time = 2, Lat = 3, Lon = 4)
-  df.gps$Bird <- files[j,]
-  
-  # df.gps <- df.gps[1:10000,] # use to test code
-  
-  # ~~~~~~~~
-  
-  # Edit time format ####
-  
-  df.gps$Date <- dmy(df.gps$Date)
-  df.gps$Date.Time <- as.POSIXct(paste(df.gps$Date, as.character(df.gps$Time)), format = "%Y-%m-%d %H:%M:%S", tz="UTC")
-  
-  # ~~~~~~~~
-  
-  # Interpolation ####
-  
-  tr <- as.ltraj(data.frame(cbind(df.gps$Lon, df.gps$Lat)),
-                 df.gps$Date.Time, df.gps$Bird)
-  df.gps <- redisltraj(tr, u = 1, type = "time") # u = new step length in seconds
-  df.gps <- ld(df.gps) %>%
-    dplyr::select(x:date) %>%
-    dplyr::rename(Lon = 1, Lat = 2, DateTime = 3)
-  
+    dplyr::rename(Date = 1, Time = 2, Lat = 3, Lon = 4) %>%
+    mutate(Bird = files[j,]) %>%
+    mutate(Date = dmy(Date)) %>%
+    mutate(DateTime = as.POSIXct(paste(Date, as.character(Time)), format = "%Y-%m-%d %H:%M:%S", tz="Etc/GMT-6"))
+
   # ~~~~~~~~
 
   # Cut out data pre- first departure and post- last return #####
@@ -112,9 +94,10 @@ for (j in 1:nrow(files)) {
   
   # Combine trip/nest and duration:
   
-  All.info <- as.data.frame(cbind(Trip, Duration, Max.Dist))
-  All.info$Duration <- as.numeric(as.character(All.info$Duration))
-  All.info$Max.Dist <- as.numeric(as.character(All.info$Max.Dist))
+  All.info <- as.data.frame(cbind(Trip, Duration, Max.Dist)) %>%
+    mutate(Duration = as.numeric(as.character(Duration))) %>%
+    mutate(Max.Dist = as.numeric(as.character(Max.Dist)))
+  
   All.info$Row.n <- seq.int(nrow(All.info))
   
   # If trip < 30 mins, make non-trip
@@ -142,7 +125,126 @@ for (j in 1:nrow(files)) {
   # Add trip info data back into normal data
   
   # Add trip info start and end times
-  Times <- lapply(split, "[", , "DateTime")
+  Times <- do.call(c, lapply(split, "[", "DateTime"))
+  All.info$StartTime <- do.call(c, (lapply(Times, head, 1)))
+  All.info$EndTime <- do.call(c, (lapply(Times, tail, 1)))
+  All.info <- as.data.table(All.info)
+  # Create "end time" for trip data
+  df.gps.dt <- as.data.table(df.gps)
+  df.gps.dt$EndTime <- df.gps.dt$DateTime + as.numeric(mean(diff(df.gps$DateTime)))
+  names(df.gps.dt)[6]<-"StartTime"
+  # Set key on dt2
+  setkey(All.info, StartTime, EndTime)
+  # Do the join
+  df.gps.dt <- foverlaps(df.gps.dt,
+                         All.info,
+                         type = "any", mult="first") %>%
+    dplyr::select(i.StartTime, Lon, Lat, Trip, Dist.km, TripID, Bird) %>%
+    dplyr::rename(DateTime = 1)
+  
+  # ~~~~~~~~
+  
+  # Write file ####
+  
+  write_csv(df.gps.dt, paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[j,], "_gps.csv"))
+
+  # ~~~~~~~~
+  
+  # Interpolation ####
+  
+  tr <- as.ltraj(data.frame(cbind(df.gps.dt$Lon, df.gps.dt$Lat)),
+                 df.gps.dt$DateTime, df.gps.dt$Bird)
+  df.gps <- redisltraj(tr, u = 1, type = "time") # u = new step length in seconds
+  df.gps <- ld(df.gps) %>%
+    dplyr::select(x:date) %>%
+    dplyr::rename(Lon = 1, Lat = 2, DateTime = 3)
+  
+  # ~~~~~~~~
+  
+  # Cut out data pre- first departure and post- last return #####
+  
+  # Is coordinate at the colony, or away?
+  
+  home.or.away = point.in.polygon(df.gps$Lon, df.gps$Lat, 
+                                  diego.garcia[,'lon'], diego.garcia[,'lat'], 
+                                  mode.checked=FALSE)
+  
+  start = df.gps$DateTime[min(which(home.or.away == 0))]  # first departure
+  finish = df.gps$DateTime[max(which(home.or.away == 0))]  # last return
+  
+  # Trim and subset
+  df.gps <- df.gps[which(df.gps$DateTime > start & df.gps$DateTime < finish),]
+  
+  # ~~~~~~~~
+  
+  # >1 km from colony = trip ####
+  
+  # Determine nest coordinates (as the most common GPS)
+  
+  nest.coords = round(c(getmode(df.gps$Lon), getmode(df.gps$Lat)), digits = 3)
+  
+  # Calc dists from nest
+  
+  df.gps$Dist.km = distHaversine(nest.coords, cbind(df.gps$Lon, df.gps$Lat))/1000
+  
+  df.gps$Trip <- ifelse(df.gps$Dist.km > 1,
+                        TRUE,
+                        FALSE)
+  
+  # ~~~~~~~~
+  
+  # Assign trip IDs and re-assign short (< 30 min) trips ####
+  
+  # Split up data frame where value of Trip factor changes:
+  
+  split <- split(df.gps, cumsum(1:nrow(df.gps) %in%
+                                  which(df.gps$Trip != dplyr::lag(df.gps$Trip))))
+  
+  # Extract whether time period is a trip or at the nest:
+  
+  Trip <- as.character(unlist(lapply(split, "[", 1, "Trip")))
+  
+  # Calculate duration of time period:
+  
+  Duration <- unname(unlist(lapply(split, nrow)) * as.numeric(mean(diff(df.gps$DateTime))))
+  
+  # Calculate max dist from the nest:
+  
+  Max.Dist <- unlist(lapply(lapply(split, "[[", 4), max))
+  
+  # Combine trip/nest and duration:
+  
+  All.info <- as.data.frame(cbind(Trip, Duration, Max.Dist))
+  All.info$Duration <- as.numeric(as.character(All.info$Duration))
+  All.info$Max.Dist <- as.numeric(as.character(All.info$Max.Dist))
+  All.info$Row.n <- seq.int(nrow(All.info))
+  
+  # If trip < 30 mins, make non-trip
+  
+  All.info$Trip <- ifelse(All.info$Trip == "TRUE" &
+                            All.info$Duration < 1800,
+                          "FALSE", All.info$Trip)
+  
+  # If trip < 1 km, make non-trip
+  
+  All.info$Trip <- ifelse(All.info$Trip == "TRUE" &
+                            All.info$Trip < 1,
+                          "FALSE", All.info$Trip)
+  
+  # Assign trip IDs to TRUE trips
+  
+  Colony.info <- All.info[which(All.info$Trip=='FALSE'),]
+  Colony.info$TripID <- NA
+  Trip.info <- All.info[which(All.info$Trip=='TRUE'),]
+  Trip.info$TripID <- seq.int(nrow(Trip.info))
+  
+  All.info <- rbind(Colony.info, Trip.info)
+  All.info <- All.info[order(All.info$Row.n),]
+  
+  # Add trip info data back into normal data
+  
+  # Add trip info start and end times
+  Times <- lapply(split, "[", "DateTime")
   All.info$StartTime <- do.call(c, (lapply(Times, head, 1)))
   All.info$EndTime <- do.call(c, (lapply(Times, tail, 1)))
   All.info <- as.data.table(All.info)
@@ -163,8 +265,8 @@ for (j in 1:nrow(files)) {
   
   # Write file ####
   
-  write_csv(df.gps.dt, paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[j,], "_gps.csv"))
-
+  write_csv(df.gps.dt, paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[j,], "_gps_interp.csv"))
+  
   }
 
 rm(All.info, Colony.info, Trip.info, df.gps, split, Times, tr, Duration, nest.coords, Trip, j)
