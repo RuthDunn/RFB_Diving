375495208027f2c7b1805e9eed36137dfdd42645
Ruth Dunn <Ruth@HPspectrex3601.win.pet.hw.ac.uk>
2023-01-25 15:05:46 +0000

    Read in intial dive data and combine it with GPS foraging trips

--- RFB_Diving_Scripts/RFB_Processing_Diving.R
+++ RFB_Diving_Scripts/RFB_Processing_Diving.R
@@ -1,11 +1,10 @@
 rm(list = ls(all = TRUE))
 
 library(tidyverse) # for piping etc
-# library(lubridate) # for dmy function
-# library(adehabitatHR) # for interpolation
 library(data.table) # for converting df to dt
 library(diveMove) # for extracting dive stats
 library(sf) # for loading and plotting the Chagos shapefile
+library(suncalc) # for calculating times of day
 
 chagos = read_sf("RFB_Diving_Data/Chagos_Maps/chagos_maps/Chagos_v6_land_simple.shp")
 
@@ -34,12 +33,14 @@ for (i in 1:nrow(files)) {
   
   # Load data ####
   
+  filename <- paste0("RFB_Diving_Data/BIOT_AxyTrek_Dives_csv/", files[i,], ".csv")
+  
   df <- left_join(read_tsv(paste0("RFB_Diving_Data/BIOT_AxyTrek_Dives_csv/", files[i,], ".csv")) %>%
                     dplyr::select(TagID, Date, Time, Depth) %>%
                     drop_na() %>%
-                    mutate(DateTime = as.POSIXct(paste(Date, as.character(Time)), format = "%d/%m/%Y %H:%M:%S", tz="UTC")) %>%
+                    mutate(DateTime = as.POSIXct(paste(Date, as.character(Time)), format = "%d/%m/%Y %H:%M:%S", tz="Etc/GMT-6")) %>%
                     dplyr::select(TagID, DateTime, Depth),
-                  read_csv(paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[i,], "_gps.csv")) %>%
+                  read_csv(paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[i,], "_gps_interp.csv")) %>%
                     dplyr::select(DateTime, Lon, Lat, Dist.km, Trip, TripID),
                   by = "DateTime") %>%
     drop_na(Lat)
@@ -80,7 +81,7 @@ for (i in 1:nrow(files)) {
   
   # Add trip info data back into normal data
   # Add trip info start and end times
-  Times <- do.call(c, lapply(split, "[", , "DateTime"))
+  Times <- do.call(c, lapply(split, "[", "DateTime"))
   All.info$StartTime <- do.call(c, (lapply(Times, head, 1)))
   All.info$EndTime <- do.call(c, (lapply(Times, tail, 1)))
   All.info <- as.data.table(All.info)
@@ -95,32 +96,47 @@ for (i in 1:nrow(files)) {
                          All.info,
                          type = "any", mult="first") %>%
     dplyr::select(i.StartTime, Lon, Lat, Trip, Dist.km, TripID, Depth_mod) %>%
-    dplyr::rename(DateTime = 1)
+    dplyr::rename(DateTime = 1) %>%
+    mutate(date = format(DateTime, format = "%Y-%m-%d")) %>%
+    mutate(date = ymd(date)) %>%
+    rename(lat = Lat, lon = Lon) %>%
+    drop_na()
   
-  df.dt <- na.omit(df.dt)
+  # Include a 'time of day' column ####
+  
+  df.dt <- cbind(df.dt, getSunlightTimes(data = df.dt, keep = c("dawn", "sunrise", "sunset", "dusk"), tz = "Etc/GMT-6")[,c(4:7)])
+  
+  df.dt$ToD = ifelse(df.dt$DateTime <= df.dt$dawn | df.dt$DateTime > df.dt$dusk, "Night",
+                  ifelse(df.dt$DateTime <= df.dt$sunrise & df.dt$DateTime > df.dt$dawn, "Dawn",
+                         ifelse(df.dt$DateTime > df.dt$sunrise & df.dt$DateTime <= df.dt$sunset, "Day",
+                                ifelse(df.dt$DateTime > df.dt$sunset & df.dt$DateTime <= df.dt$dusk, "Dusk", NA))))
+  
+  df.dt <- df.dt %>%
+    rename(Lat = lat, Lon = lon) %>%
+    dplyr::select(DateTime, Lon, Lat, Trip, Dist.km, TripID, Depth_mod, ToD)
   
   # Save dive plots for each trip ####
   
   df.dt$Dive <- ifelse(df.dt$Depth_mod > 0.1, TRUE, FALSE)
   
-  for (j in 1:length(unique(df$TripID))) {
-    
+  for (j in 1:length(unique(df.dt$TripID))) {
+
     # j = 1
-    
+
     tripj <- unique(df.dt$TripID)[j]
-    
+
     ggplot(subset(df.dt, TripID == tripj)) +
       geom_point(aes(x = DateTime, y = Depth_mod, col = as.factor(Dive)), alpha = 0.4, show.legend = F) +
       scale_colour_manual(values = c("#FFB000", "#DC267F")) +
-      theme_light() +
-      scale_y_continuous(breaks = c(0.1,0.2,0.3,0.4,0.5))
-    
-    ggsave(paste0("RFB_Diving_Plots/Dive", files[i,], "_", j, ".png"), width = 8, height = 8)
+      theme_light()
+
+    ggsave(paste0("RFB_Diving_Plots/", files[i,], "_", j, "dives.png"), width = 8, height = 8)
   }
+
+  write_csv(df, paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[i,], "_depth_corrected.csv"))
   
   # Use diveMove to extract dive stats ####
   
-  filename <- paste0("RFB_Diving_Data/BIOT_AxyTrek_Dives_csv/", files[i,], ".csv")
   divesTDR <- createTDR(df.dt$DateTime,
                         depth = df.dt$Depth_mod,
                         dtime = 1,
@@ -130,36 +146,32 @@ for (i in 1:nrow(files)) {
   caldivesTDR <- calibrateDepth(divesTDR, dive.thr = 0.1, zoc.method = "offset", offset = 0)
   
   df <- right_join(diveStats(caldivesTDR) %>%
-                        select(begdesc, divetim, maxdep) %>%
+                        dplyr::select(begdesc, divetim, maxdep) %>%
                         rename(DateTime = 1, Duration = 2, MaxDepth = 3),
                       df.dt %>%
-                        select(DateTime, Lon, Lat, Dist.km, TripID, Dive),
+                     dplyr::select(DateTime, Lon, Lat, Dist.km, TripID, Dive, ToD),
                       by = "DateTime")
   
   # Save location plots for each trip ####
   
   for (j in 1:length(unique(df$TripID))) {
-    
+
     # j = 1
-    
+
     tripj <- unique(df$TripID)[j]
-    
+
     ggplot(subset(df, TripID == tripj)) +
       geom_point(aes(x = Lon, y = Lat, col = as.factor(Dive), size = as.factor(Dive)), alpha = 0.4, show.legend = F) +
       scale_colour_manual(values = c("#FFB000", "#DC267F")) +
       scale_size_manual(values = c(0.2,2)) +
       geom_sf(data = chagos, fill = "#009E73", col = "#009E73") +
       theme_light()
-    
-    ggsave(paste0("RFB_Diving_Plots/", files[i,], "_", j, ".png"), width = 8, height = 8)
+
+    ggsave(paste0("RFB_Diving_Plots/", files[i,], "_", j, "map.png"), width = 8, height = 8)
   }
   
   # Write file ####
   
-  write_csv(df, paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/Map", files[i,], "_depth_corrected.csv"))
-  
-  # Save plots of each trip ####
-
-  
+  write_csv(df, paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[i,], "_dive_stats.csv"))
   
 }
