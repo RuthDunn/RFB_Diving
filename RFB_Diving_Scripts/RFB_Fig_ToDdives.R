b9d71930fee92a06973bdffe9cd37391a33e602f
Ruth Dunn <ruthelizabethdunn@gmail.com>
2023-02-08 10:00:20 +0000

    Change colour palette

--- RFB_Diving_Scripts/RFB_Fig_ToDdives.R
+++ RFB_Diving_Scripts/RFB_Fig_ToDdives.R
@@ -11,6 +11,8 @@ chagos = read_sf("RFB_Diving_Data/Chagos_Maps/chagos_maps/Chagos_v6_land_simple.
 files <- as.data.frame(list.files(path = "RFB_Diving_Data/BIOT_AxyTrek_Processed/", pattern = "*_dive_stats.csv")) %>%
   separate(1, into = "files", sep = "_dive_stats.csv")
 
+# Colour palette: https://personal.sron.nl/~pault/#sec:qualitative
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Loop through .csv files to load data ####
@@ -54,20 +56,20 @@ df.dives$Hour <- ordered(df.dives$Hour, levels = c("00", "01", "02", "03", "04",
 a <- ggplot(df.dives, aes(x = Hour, y = Dive)) +
       # geom_rect(aes(xmin = "00", xmax = "24", ymin = -Inf, ymax = Inf,
       #               fill = Hour_shade), alpha = .2) +
-  geom_bar(aes(y = (after_stat(count))/sum(after_stat(count))), fill = "#785EF0") +
+  geom_bar(aes(y = (after_stat(count))/sum(after_stat(count))), fill = "#DDAA33") +
   scale_y_continuous(labels=scales::percent) +
   scale_fill_discrete(drop=FALSE) +
   scale_x_discrete(drop=FALSE) +
   theme_light() %+replace% theme(axis.title.x = element_blank(),
                                  axis.ticks.x = element_blank(),
-                                 axis.text.x = element_bla) +
+                                 axis.text.x = element_blank()) +
   ylab("Relative frequency of dive events")
 
 # Plots the dive depths at different times of day ####
 
 b <- ggplot(df.dives, aes(x = Hour, y = MaxDepth)) +
   geom_jitter(col = "grey70", alpha = 0.3) +
-  geom_violin(fill = NA, col = "#785EF0") +
+  geom_violin(fill = NA, col = "#DDAA33") +
   scale_fill_discrete(drop=FALSE) +
   scale_x_discrete(drop=FALSE) +
   theme_light() +
