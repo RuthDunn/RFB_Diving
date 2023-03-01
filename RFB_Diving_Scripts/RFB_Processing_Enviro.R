f0aaf32c6b7a8181ebfa09d3d7b9cdc303a00228
Ruth Dunn <Ruth@HPspectrex3601.win.pet.hw.ac.uk>
2023-03-01 14:24:23 +0000

    First attempt at extracting environmental data from real dive locations and two sets of pseudo locations.

---
+++ RFB_Diving_Scripts/RFB_Processing_Enviro.R
@@ -0,0 +1,116 @@
+
+library(tidyverse) # for general working
+library(adehabitatHR) # for creating MCPs
+library(sf) # for creating sf polygons and points
+library(sfheaders) # for creating sf lines
+
+rm(list = ls(all = TRUE))
+
+files <- as.data.frame(list.files(path = "RFB_Diving_Data/BIOT_AxyTrek_Dives_csv/", pattern = "*.csv")) %>%
+  separate(1, into = "files", sep = ".csv")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Select the environmental data that I want:
+# (Guide: https://cran.r-project.org/web/packages/rerddapXtracto/vignettes/UsingrerddapXtracto.html)
+
+# MODIS AQUAL daytime SST data:
+sstInfo <- rerddap::info('nasa_jpl_1ce2_ad3f_9bae')
+sstInfo
+
+# GEBCO bathymetry data:
+bathyInfo <- rerddap::info('GEBCO_2020_Lon0360')
+bathyInfo
+
+# 
+chlorInfo <- info('pmlEsaCCI50OceanColorDaily')
+chlorInfo
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+bird.enviro <- NULL
+all.enviro <- NULL
+
+for (i in 1:nrow(files)) {
+  
+  i = 1
+  
+  # Load data ####
+  
+  df.dives <- read_csv(paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[i,], "_dive_stats.csv")) %>%
+    mutate(BirdID = i)
+  
+  df.gps <- read_csv(paste0("RFB_Diving_Data/BIOT_AxyTrek_Processed/", files[i,], "_gps_interp.csv"))
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  # Create pseudo-absence data ####
+  
+  # Extract the same number of points as there are dives for each trip from:
+  # a) The dive track &
+  # b) The MCP.
+  
+  # Run through each trip:
+  for (j in 1:length(unique(df.gps$TripID))) {
+    
+    # j = 2
+    
+    # Create dataframe of bird id, trip id and date of Trip j, & repeat 3 times:
+    tripj.info <- bind_rows(replicate(3, subset(df.dives, TripID == unique(df.dives$TripID)[j])[,c(14, 8, 4)], simplify = F))
+    
+    # Extract current dive locations:
+    tripj.divepoints <- subset(df.dives, TripID == unique(df.dives$TripID[j]))[,c(5, 6)] %>%
+      mutate(Value = "Dive.Locs") %>%
+      rename(X = 1, Y = 2)
+        
+    # Subset the gps track of Trip j:
+    tripj <- subset(df.gps, Trip == unique(df.gps$Trip)[j])
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    # a) For each trip, extract 'non-dive' locations ####
+    
+    tripj.pspointsa <- st_line_sample(x = sf_linestring(obj = df.gps, x = "Lon", y = "Lat"),
+                                      n = nrow(tripj.info)/3, type = "random")
+    
+    tripj.pspointsa <- as.data.frame(st_coordinates(tripj.pspointsa)[,c(1:2)]) %>%
+      mutate(Value = "Trip.Locs")
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    # b)  MCP points ####
+    
+    # Create 95% MCP
+    # Extract as many random points as there are true dive locations
+    
+    tripj.pspointsb <- st_sample(x = st_as_sf(mcp(SpatialPoints(coords = cbind(tripj$Lon, tripj$Lat)), percent = 95)),
+                                 size = nrow(tripj.info)/3)
+    
+    tripj.pspointsb <- as.data.frame(st_coordinates(tripj.pspointsb)) %>%
+      mutate(Value = "Available.Locs")
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    # Combine all different point data frames together and...
+    # Extract the environmental data ####
+    
+    tripj.enviro <- cbind(tripj.info, rbind(tripj.divepoints, tripj.pspointsa, tripj.pspointsb)) %>%
+      mutate(SST = rxtracto(sstInfo, parameter = 'sst',
+                            xcoord = X, ycoord = Y, tcoord = as.Date(DateTime),
+                            xlen = .2, ylen = .2, progress_bar = TRUE)[[1]]) %>%
+      mutate(Depth = rxtracto(bathyInfo, parameter = 'elevation',
+                            xcoord = X, ycoord = Y,
+                            xlen = .2, ylen = .2, progress_bar = TRUE)[[1]]) %>%
+      mutate(Chlor = rxtracto(chlorInfo, parameter = 'chlor_a',
+                              xcoord = X, ycoord = Y, tcoord = as.Date(DateTime),
+                              xlen = .2, ylen = .2, progress_bar = TRUE)[[1]])
+    
+    bird.enviro <- rbind(bird.enviro, tripj.enviro)
+    
+  }
+ 
+  rm(tripj.divepoints, tripj.pspointsa, tripj.pspointsb, tripj, tripj.info, bathyInfo, chlorInfo, sstInfo)
+  
+  all.enviro <- rbind(all.enviro, bird.enviro) 
+  
+}
